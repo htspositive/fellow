2a2446f9fe6dd3edd82059c41c18999dfb604240
htspositive <htspositive@163.com>
2020-02-20 14:46:30 +0800

    111

--- Webpack/src/index.js
+++ Webpack/src/index.js
@@ -1,6 +1,8 @@
-//es6引入模块
+// es6引入模块的方式  ./代表当前路径
 import img from './img/1.jpg';
-console.log('sdsfdsfdadf');
+// import style from './css/index.css';
+import style from './css/test.scss';
+console.log('2222');
 var oImg = new Image();
-oImg.src = img;
+oImg.src= img;
 document.body.append(oImg);
--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -1,5 +1,7 @@
 // node引入模块的方式
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 module.exports = {
     // 入口文件 
     entry:"./src/index.js",
@@ -11,12 +13,46 @@ module.exports = {
     },
     module:{
         rules:[
-        {
-            test:/\.(jpg|png|gif)$/,
-            use:{
-                loader:'url-loader'
+            {
+                test:/\.(jpg|png|gif)$/,
+                use:{
+                    loader:'url-loader',
+                    options:{
+                        outputPath:'images/',
+                        limit:100,
+                        name:'[name].[ext]'
+                    }
+                }
+            },
+            {
+                test:/\.css$/,
+                use:[ //loader的执行顺序从后向前
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                test:/\.scss$/,
+                use:[ //loader的执行顺序从后向前
+                    "style-loader",
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
             }
-        }
-               ]
+        ]
+    },
+    plugins:[
+        // template中的src/indx.html作为模板生成dist目录，并把打包生成的js注入到html中
+        new HtmlWebpackPlugin({
+            template:"./src/index.html"
+        }),
+        // 打包之前先将dist目录删除
+        new CleanWebpackPlugin()
+    ],
+    devServer:{
+        contentBase:path.resolve(__dirname,"dist"),
+        host:'localhost',
+        // port:
     }
 }
